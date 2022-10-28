22d7588f7fe594fc96475b22085646c4e0a9ba04
Ethan Swafford <ethanswafford@yahoo.com>
2022-10-28 03:24:27 -0500

    devPush

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+let humanDiff;
+
+
 function generateTarget() {
     return Math.round(Math.random() * 10);
+}
+
+function compareGuesses(userGuess, computerGuess, targetGuess) {
+    humanDiff = Math.abs(targetGuess - userGuess);
 }
 
 console.log(generateTarget());
